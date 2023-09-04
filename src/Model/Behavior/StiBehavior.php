9b50c67da083a9d7bb938c56f2391e9b8e04d00b
Adam Rusinowski <adam.rusinowski@gmail.com>
2023-09-04 11:50:32 +0200

    cc

--- src/Model/Behavior/StiBehavior.php
+++ src/Model/Behavior/StiBehavior.php
@@ -16,12 +16,15 @@ use Cake\Utility\Inflector;
 use Cake\Validation\Validator;
 use Exception;
 
+/**
+ * StiBehavior class
+ */
 class StiBehavior extends Behavior
 {
     /**
      * Default config
      *
-     * @var array
+     * @var array<string, mixed>
      */
     protected array $_defaultConfig = [
         'table' => null,
@@ -41,7 +44,6 @@ class StiBehavior extends Behavior
      *
      * @param array $config Configuration options passed to the constructor
      * @return void
-     *
      * @throws \Exception If the Entity isn't using the trait \Muffin\Sti\Model\Entity\StiAwareTrait
      */
     public function initialize(array $config): void
@@ -54,6 +56,9 @@ class StiBehavior extends Behavior
             $this->_table()->setEntityClass($defaultEntityClass);
         }
 
+        /**
+         * @psalm-suppress ArgumentTypeCoercion
+         */
         if (!method_exists($defaultEntityClass, 'forCopy')) {
             throw new Exception($defaultEntityClass . ' is not using the StiAwareTrait');
         }
@@ -63,7 +68,6 @@ class StiBehavior extends Behavior
      * Verifies the configuration of the Behavior
      *
      * @return void
-     *
      * @throws \Exception
      */
     public function verifyConfig(): void
@@ -91,11 +95,10 @@ class StiBehavior extends Behavior
      *
      * @param string $name Method name being called
      * @param array $args arguments passed to the original method
-     *
      * @return mixed
      * @throws \Exception
      */
-    public function __call(string $name, array $args)
+    public function __call(string $name, array $args): mixed
     {
         $type = Inflector::underscore(substr($name, 3));
 
@@ -112,9 +115,7 @@ class StiBehavior extends Behavior
      * Gets the real table
      *
      * @param string|null $key Table to find
-     *
      * @return \Cake\ORM\Table
-     *
      * @throws \Exception
      */
     protected function _table(?string $key = null): Table
@@ -142,7 +143,6 @@ class StiBehavior extends Behavior
      * @param \Cake\ORM\Query\SelectQuery $query Quey
      * @param \ArrayObject $options Options
      * @param bool $primary If primary
-     *
      * @return void
      */
     public function beforeFind(EventInterface $event, SelectQuery $query, ArrayObject $options, bool $primary): void
@@ -151,7 +151,7 @@ class StiBehavior extends Behavior
             return;
         }
 
-        $query->formatResults(fn(CollectionInterface $results): CollectionInterface => $results
+        $query->formatResults(fn (CollectionInterface $results): CollectionInterface => $results
             ->map(function ($row) {
                 $type = $row[$this->getConfig('typeField')];
                 $entityClass = $this->_typeMap[$type]['entityClass'];
@@ -169,9 +169,7 @@ class StiBehavior extends Behavior
      * @param \Cake\Event\EventInterface $event Event
      * @param \Cake\Validation\Validator $validator Original Validator
      * @param string $name Name of validator that is being built
-     *
      * @return void
-     *
      * @throws \Exception
      */
     public function buildValidator(EventInterface $event, Validator $validator, string $name): void
@@ -202,9 +200,7 @@ class StiBehavior extends Behavior
      * @param \Cake\Event\EventInterface $event Event
      * @param \Cake\Datasource\EntityInterface $entity Entity to save
      * @param \ArrayObject $options Options
-     *
      * @return void
-     *
      * @throws \Exception
      */
     public function beforeSave(EventInterface $event, EntityInterface $entity, ArrayObject $options): void
@@ -226,9 +222,7 @@ class StiBehavior extends Behavior
      * @param \Cake\Event\EventInterface $event Event
      * @param \ArrayObject $data Data to marshall
      * @param \ArrayObject $options Options
-     *
      * @return void
-     *
      * @throws \Exception
      */
     public function beforeMarshal(EventInterface $event, ArrayObject $data, ArrayObject $options): void
@@ -247,11 +241,10 @@ class StiBehavior extends Behavior
 
     /**
      * Adds the type and implements magic newXxx method
+     *
      * @param string $key Key
      * @param string $entityClass Entity to add
-     *
      * @return void
-     *
      * @throws \Exception
      */
     public function addType(string $key, string $entityClass): void
--- src/Model/Entity/StiAwareTrait.php
+++ src/Model/Entity/StiAwareTrait.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace Muffin\Sti\Model\Entity;
 
+/**
+ * StiAwareTrait
+ */
 trait StiAwareTrait
 {
     /**
